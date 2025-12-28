4f04fa227810e91e3b3f3788dc5e51a030a7e49d
Sam Noyce <samuel.noyce@outlook.com>
2025-12-27 23:30:25 -0600

    feat(main): fetch secret on startup

--- cmd/lambda/main.go
+++ cmd/lambda/main.go
@@ -1,18 +1,32 @@
 package main
 
 import (
+	"context"
 	"log"
 
 	"github.com/aws/aws-lambda-go/lambda"
 
 	"coinbase-automation/internal/config"
 	"coinbase-automation/internal/handler"
+	"coinbase-automation/internal/secret"
 )
 
 func main() {
+	ctx := context.Background()
+
 	cfg, err := config.Load()
 	if err != nil {
 		log.Fatalf("Failed to load config: %v", err)
+	}
+
+	secretClient, err := secret.NewClient(ctx)
+	if err != nil {
+		log.Fatalf("Failed to create secret client: %v", err)
+	}
+
+	s, err := secretClient.Fetch(ctx, cfg.SecretName)
+	if err != nil {
+		log.Fatalf("Failed to fetch secret: %v", err)
 	}
 
 	h := handler.New()
