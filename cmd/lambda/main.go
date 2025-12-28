01d795f583b95b1938ff9be0c18a291c57a0fc0d
Sam Noyce <samuel.noyce@outlook.com>
2025-12-27 23:18:06 -0600

    feat(main): load config on startup

--- cmd/lambda/main.go
+++ cmd/lambda/main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"log"
+
 	"github.com/aws/aws-lambda-go/lambda"
 
+	"coinbase-automation/internal/config"
 	"coinbase-automation/internal/handler"
 )
 
 func main() {
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatalf("Failed to load config: %v", err)
+	}
+
 	h := handler.New()
+
 	lambda.Start(h.Run)
 }
